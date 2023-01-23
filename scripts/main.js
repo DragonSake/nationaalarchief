2c169e091735dd5099ea13991c4bb83eedfb8da8
DragonSake <40611000+DragonSake@users.noreply.github.com>
2023-01-23 14:25:28 +0100

    progressbar

--- scripts/main.js
+++ scripts/main.js
@@ -4,149 +4,41 @@ import '../styles/style.css'
 // We can use node_modules directely in the browser!
 // import * as d3 from 'd3';
 
-// fetch("na.json")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
+// We can use node_modules directely in the browser!
+import * as d3 from "d3";
 
-// fetch("lol.json")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
+// (api)data importeren
+fetch("lol.json")
+  .then((response) => response.json())
+  .then((dataSet) => {
+    procent(dataSet); //functie uitvoeren
+  });
 
-// function kills(dataSet, player = 2) {
-//     let pieData = [dataSet[player].Kill, 100 - dataSet[player].Kill];
+// functie maken + de lengte en breedte bepalen
+function procent(dataSet) {
+  const chartWidth = 22;
+  const chartHeight = 105;
 
-//     const text = "";
-//     const width = 180;
-//     const height = 180;
-//     const thickness = 20;
-//     const radius = Math.min(width, height) / 2;
+  // X-as
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataSet, (d) => d.Percentage)])
+    .range([0, chartWidth]);
 
-//     // functie eerste waarde wordt geel, tweede waarde wordt donkerblauw
-//     function color(d, i) {
-//         if (i === 0) {
-//             return "#e7bd3b";
-//         } else {
-//             return "#131c43";
-//         }
-//     }
+  // Y-as
+  const yScale = d3
+    .scaleBand()
+    .domain(d3.map(dataSet, (d) => d.Archief))
+    .range([0, chartHeight])
+    .paddingInner(0.6);
 
-//     // selecteert de div met de class donut
-//     const svg = d3
-//         .select(".donut")
-//         .append("svg")
-//         .attr("class", "pie")
-//         .attr("width", width)
-//         .attr("height", height);
+  //maakt de bar aan
+  const bars = d3.select("#bars").selectAll("g").data(dataSet).join("g");
 
-//     const g = svg
-//         .append("g")
-//         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-//     // cirkel in de donut
-//     const arc = d3
-//         .arc()
-//         .innerRadius(radius - thickness)
-//         .outerRadius(radius);
-
-//     const pie = d3
-//         .pie()
-//         .value(function (d) {
-//             return d;
-//         })
-//         .sort(null);
-
-//     const path = g
-//         .selectAll("path")
-//         .data(pie(pieData))
-//         .enter()
-//         .append("g")
-//         .append("path")
-//         .attr("d", arc)
-//         .attr("fill", (d, i) => color(d, i))
-//         .each(function (d, i) {
-//             this._current = i;
-//         });
-
-//     svg
-//         .append("svg:text")
-//         .attr("dx", "90px")
-//         .attr("dy", "100px")
-//         .attr("text-anchor", "middle")
-//         .attr("font-size", "40")
-//         .text(dataSet[player].Kill);
-// }
-
-var data = [{
-        name: "Faker",
-        value: 60
-    },
-    {
-        name: "Others",
-        value: 40
-    },
-];
-
-var text = "";
-
-var width = 180;
-var height = 180;
-var thickness = 20;
-
-var radius = Math.min(width, height) / 2;
-//var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-function color(d) {
-    if (d.value === 60) {
-        return '#01689b'
-    } else {
-        return "#fff"
-    }
+  bars
+    .append("rect")
+    .attr("height", yScale.bandwidth())
+    .attr("width", (d) => xScale(d.Percentage))
+    .attr("y", (d) => yScale(d.Archief))
+    .attr("fill", "#C9005C")
 }
-
-var svg = d3.select("#chart")
-    .append('svg')
-    .attr('class', 'pie')
-    .attr('width', width)
-    .attr('height', height);
-
-var svg2 = d3.select("#chart2")
-    .append('svg')
-    .attr('class', 'pie')
-    .attr('width', width)
-    .attr('height', height);
-
-var g = svg.append('g')
-    .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
-
-var arc = d3.arc()
-    .innerRadius(radius - thickness)
-    .outerRadius(radius);
-
-var pie = d3.pie()
-    .value(function (d) {
-        return d.value;
-    })
-    .sort(null);
-
-var path = g.selectAll('path')
-    .data(pie(data))
-    .enter()
-    .append("g")
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', d => color(d))
-    .each(function (d, i) {
-        this._current = i;
-    });
-
-g.append('text')
-    .attr('text-anchor', 'middle')
-    .attr('dy', '.35em')
-    .text(text);
-
-svg.append("svg:text")
-    .attr("dy", "110px")
-    .attr("dx", "100px")
-    .attr("text-anchor", "middle")
-    .attr("font-size", "40")
-    .text("60%");
